eb92c5e18c97115287e86ea8d3e8b7fada707c2b
floyzg <floyzg@gmail.com>
2025-11-29 03:34:12 +0400

    feat (client): initialize React + Vite project with routing, axios, metadata, and base structure

---
+++ client/src/router/routes.jsx
@@ -0,0 +1,7 @@
+import Home from "../pages/Home/Home";
+import News from "../pages/News/News";
+
+export const routes = [
+  { path: "/", element: <Home /> },
+  {path: "/news", element: <News /> },
+];
