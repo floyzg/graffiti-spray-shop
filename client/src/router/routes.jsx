152aa8a06d4eaf8806df68a1d02a829f97f74d55
floyzg <floyzg@gmail.com>
2025-12-07 15:44:01 +0400

    feat (client): #3 Settings deploy for gh-pages

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -4,6 +4,7 @@ import Auth from "../pages/Auth/Auth";
 import Register from "../pages/Register/Register";
 import ResetPassword from "../pages/ResetPassword/ResetPassword";
 import Cart from "../pages/Cart/Cart";
+import { Navigate } from "react-router-dom";
 
 export const routes = [
   { path: "/", element: <Home /> },
@@ -12,4 +13,6 @@ export const routes = [
   { path: "/sign-up", element: <Register /> },
   { path: "/reset-password", element: <ResetPassword /> },
   { path: "/cart", element: <Cart /> },
+
+  { path: "*", element: <Navigate to="/" /> },
 ];
